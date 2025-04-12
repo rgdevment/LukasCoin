8ce5257a65cc14971ec3670384a556eec8c0a9d7
rgdevment <mhidalgog@falabella.com>
2025-04-12 17:18:30 -0400

    feat: simplify contracts

--- contracts/upgrades/LukasV1.sol
+++ contracts/upgrades/LukasV1.sol
@@ -1,17 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
 
 contract LukasV1 is
 Initializable,
-ERC20VotesUpgradeable,
-ERC20PermitUpgradeable,
+ERC20Upgradeable,
 ERC20BurnableUpgradeable,
 AccessControlUpgradeable,
 PausableUpgradeable,
@@ -22,7 +20,7 @@ UUPSUpgradeable
     bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
 
     uint256 public constant MAX_SUPPLY = 12_000_000 * 10 ** 18;
-    uint256 public constant INITIAL_SUPPLY = 1_200_000 * 10 ** 18;
+    uint256 public constant INITIAL_SUPPLY = 1_000_000 * 10 ** 18;
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
@@ -32,8 +30,6 @@ UUPSUpgradeable
     function initialize(address initialAdmin) public initializer {
         __ERC20_init("Lukas", "LKS");
         __ERC20Burnable_init();
-        __ERC20Permit_init("Lukas");
-        __ERC20Votes_init();
         __Pausable_init();
         __AccessControl_init();
         __UUPSUpgradeable_init();
@@ -59,16 +55,12 @@ UUPSUpgradeable
         _unpause();
     }
 
-    function _update(address from, address to, uint256 value) internal override(ERC20Upgradeable, ERC20VotesUpgradeable) {
-        super._update(from, to, value);
+    function _update(address from, address to, uint256 amount) internal override(ERC20Upgradeable) {
         require(!paused(), "LKS: Transfers paused");
+        super._update(from, to, amount);
     }
 
-    function _authorizeUpgrade(address newImplementation) internal override onlyRole(UPGRADER_ROLE) {}
-
-    function nonces(address owner) public view override(ERC20PermitUpgradeable, NoncesUpgradeable) returns (uint256) {
-        return super.nonces(owner);
-    }
+    function _authorizeUpgrade(address) internal override onlyRole(UPGRADER_ROLE) {}
 
     function supportsInterface(bytes4 interfaceId) public view override(AccessControlUpgradeable) returns (bool) {
         return super.supportsInterface(interfaceId);
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,31 +1,27 @@
 import { viem } from "hardhat";
 import { encodeFunctionData } from "viem";
-import {config as dotenvConfig} from 'dotenv';
-
-dotenvConfig();
 
 async function main() {
-    const proxyAdmin = await viem.deployContract("ProxyAdmin");
-    console.log(`ProxyAdmin: ${proxyAdmin.address}`);
-
     const impl = await viem.deployContract("LukasV1");
-    console.log(`Implementation: ${impl.address}`);
+    console.log("Implementation:", impl.address);
 
     const initializeData = encodeFunctionData({
         abi: impl.abi,
         functionName: "initialize",
-        args: [process.env.LKS_WALLET as `0x${string}`]
+        args: [process.env.LKS_WALLET]
     });
 
     console.log("initializeData:", initializeData);
+
+    const proxyAdmin = await viem.deployContract("ProxyAdmin");
+    console.log("ProxyAdmin:", proxyAdmin.address);
 
     const proxy = await viem.deployContract("TransparentUpgradeableProxy", [
         impl.address,
         proxyAdmin.address,
         initializeData
     ]);
-
-    console.log(`Proxy: ${proxy.address}`);
+    console.log("Proxy:", proxy.address);
 }
 
 main().catch(console.error);
